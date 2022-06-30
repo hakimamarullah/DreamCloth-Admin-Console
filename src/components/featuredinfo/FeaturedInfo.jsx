0a17a6e8de655f62000eaeb7f1e0e65346f9698c
Capt. Hakim Amarullah <61151625+hakimamarullah@users.noreply.github.com>
2022-06-30 11:23:44 +0700

    Homepage (#3)
    * Add components: Topbar
    
    * Add component: Sidebar
    
    * Add featuredInfo component
    
    Co-authored-by: hakimamarullah <hakimaamarullah@gmail.com>

---
+++ src/components/featuredinfo/FeaturedInfo.jsx
@@ -0,0 +1,43 @@
+import './featuredInfo.css';
+import { ArrowDropDown, ArrowDropUp } from '@material-ui/icons';
+const FeaturedInfo = () => {
+  return (
+    <div className='featured'>
+      <div className='featuredItem'>
+        <span className='featuredTitle'>Revenue</span>
+        <div className='featuredMoneyContainer'>
+          <span className='featuredMoney'>Rp200.000</span>
+          <span className='featuredMoneyRate'>
+            -10
+            <ArrowDropDown className='featuredIcon negative'/>
+          </span>
+        </div>
+        <span className='featuredSubtitle'>Compared to last month</span>
+      </div>
+      <div className='featuredItem'>
+        <span className='featuredTitle'>Sales</span>
+        <div className='featuredMoneyContainer'>
+          <span className='featuredMoney'>Rp2000.000</span>
+          <span className='featuredMoneyRate'>
+            -11
+            <ArrowDropDown className='featuredIcon negative'/>
+          </span>
+        </div>
+        <span className='featuredSubtitle'>Compared to last month</span>
+      </div>
+      <div className='featuredItem'>
+        <span className='featuredTitle'>Cost</span>
+        <div className='featuredMoneyContainer'>
+          <span className='featuredMoney'>Rp200.000</span>
+          <span className='featuredMoneyRate'>
+            +20
+            <ArrowDropUp className='featuredIcon'/>
+          </span>
+        </div>
+        <span className='featuredSubtitle'>Compared to last month</span>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedInfo;
