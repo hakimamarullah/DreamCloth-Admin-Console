0ae8aacb0eed3be72d7fd4d386fa68ff1c235744
Capt. Hakim Amarullah <61151625+hakimamarullah@users.noreply.github.com>
2022-06-30 11:24:18 +0700

    Development (#4)
    * Add components: Topbar (#1)
    
    Co-authored-by: hakimamarullah <hakimaamarullah@gmail.com>
    
    * Sidebar (#2)
    
    * Add components: Topbar
    
    * Add component: Sidebar
    
    Co-authored-by: hakimamarullah <hakimaamarullah@gmail.com>
    
    * Homepage (#3)
    
    * Add components: Topbar
    
    * Add component: Sidebar
    
    * Add featuredInfo component
    
    Co-authored-by: hakimamarullah <hakimaamarullah@gmail.com>
    
    Co-authored-by: hakimamarullah <hakimaamarullah@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Topbar from './components/topbar/Topbar'
+import Sidebar from './components/sidebar/Sidebar'
+import Home from './pages/home/Home'
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Topbar/>
+      <div className='container'>
+        <Sidebar/>
+        <Home/>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
