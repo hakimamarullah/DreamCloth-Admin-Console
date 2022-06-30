3cef208dae62f27af37c6e04bc6c5459f6d12c73
Capt. Hakim Amarullah <61151625+hakimamarullah@users.noreply.github.com>
2022-06-30 17:49:09 +0700

    Home widget (#6)
    * Chart component
    
    * Add homeWidget and UserList page
    
    Co-authored-by: hakimamarullah <hakimaamarullah@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React from 'react'
 import Topbar from './components/topbar/Topbar'
 import Sidebar from './components/sidebar/Sidebar'
 import Home from './pages/home/Home'
+import UserList from './pages/userList/UserList'
 import './App.css'
-
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 const App = () => {
   return (
-    <div>
+    <Router>
       <Topbar/>
       <div className='container'>
         <Sidebar/>
-        <Home/>
-        <div className='others'></div>
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+          <Route path='/users' element={<UserList/>}/>
+        </Routes>
       </div>
-    </div>
+    </Router>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import Chart from '../../components/chart/Chart';
 import FeaturedInfo from '../../components/featuredinfo/FeaturedInfo';
 import './home.css';
-
+import { data } from '../../data';
+import WidgetSm from '../../components/widgetSm/WidgetSm';
+import WidgetLg from '../../components/widgetLg/WidgetLg';
 const Home = () => {
   return (
     <div className='home'>
       <FeaturedInfo />
+      <Chart title='User Analytics' data={data} dataKey='Active User'/>
+      <div className="widgetContainer">
+        <WidgetSm/>
+        <WidgetLg/>
+      </div>
     </div>
   );
 };
