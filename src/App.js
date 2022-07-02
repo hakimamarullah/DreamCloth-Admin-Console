69d0c8d4e19830669cba82ce86b39e4d5351f43f
hakimamarullah <hakimaamarullah@gmail.com>
2022-07-02 11:37:09 +0700

    prepare Login development

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import React from 'react'
-import Topbar from './components/topbar/Topbar'
-import Sidebar from './components/sidebar/Sidebar'
-import Home from './pages/home/Home'
-import UserList from './pages/userList/UserList'
-import './App.css'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import User from './pages/user/User'
-import NewUser from './pages/newUser/NewUser'
-import ProductList from './pages/productList/ProductList'
-import Product from './pages/product/Product'
+import React from 'react';
+import Topbar from './components/topbar/Topbar';
+import Sidebar from './components/sidebar/Sidebar';
+import Home from './pages/home/Home';
+import UserList from './pages/userList/UserList';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import User from './pages/user/User';
+import NewUser from './pages/newUser/NewUser';
+import ProductList from './pages/productList/ProductList';
+import Product from './pages/product/Product';
+import NewProduct from './pages/newProduct/NewProduct';
 
 const App = () => {
   return (
     <Router>
-      <Topbar/>
+      <Topbar />
       <div className='container'>
-        <Sidebar/>
+        <Sidebar />
         <Routes>
-          <Route exact path='/' element={<Home/>}/>
-          <Route path='/users' element={<UserList/>}/>
-          <Route path='/user/:userId' element={<User/>}/>
-          <Route path='/newUser' element={<NewUser/>}/>
-          <Route path='/products' element={<ProductList/>}/>
-          <Route path='/product/:productId' element={<Product/>}/>
-          <Route path='/newProduct' element={<NewUser/>}/>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/users' element={<UserList />} />
+          <Route path='/user/:userId' element={<User />} />
+          <Route path='/newUser' element={<NewUser />} />
+          <Route path='/products' element={<ProductList />} />
+          <Route path='/product/:productId' element={<Product />} />
+          <Route path='/newProduct' element={<NewProduct />} />
         </Routes>
       </div>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
